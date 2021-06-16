9adcd7f18266c0fe5c940f0df1095d48ca683eee
Jesus <salcido.jesus.j@gmail.com>
2021-06-16 11:14:02 -0600

    video 2, segunda pantalla de descarga

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, send_file
 from pytube import YouTube
 
 app = Flask(__name__)
@@ -6,11 +6,20 @@ app.config['SECRET_KEY'] = "HolaGente"
 
 def download(url):
      video = url.streams.first()
-     video.download()
+     filepath = video.download()
+     return filepath
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         url = YouTube(request.form.get('url'))
-        download(url)
+        filepath = download(url)
+        return render_template('ver_video.html', filetitle = url.title, filepath = filepath)
     return render_template('index.html')
+
+@app.route('/download', methods=['GET', 'POST'])
+def descargar_video():
+    if request.method == 'POST':
+        filepath = request.form.get('filepath')
+        return send_file(filepath, as_attachment = True)
+    return redirect(url_for('index'))
